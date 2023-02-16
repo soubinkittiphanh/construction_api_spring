d3367ac1fb19917aab72de5ed1a9cb9053041dbf
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2023-02-16 14:14:47 +0700

    first

---
+++ src/test/java/com/construction/equiptment/broker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.construction.equiptment.broker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
